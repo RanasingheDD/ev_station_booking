8d1b6b8f2c9a9af090d38b2ae4853e9f27fbe507
RanasingheDD <dishand398@gmail.com>
2025-10-19 06:19:39 +0000

    login_page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import EVHubDashboard from './components/dashboard/EVHubDashboard'
+import EVAuth from './components/login/EVAuth'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <EVHubDashboard/>
+    <>     
+      <EVAuth/>
     </>
   )
 }
