54b808f3c3d304cc1d34c23fe03e28db2984d6e5
roxchain12345 <roxchain12345@gmail.com>
2025-11-06 14:06:13 +0000

    ignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import { useState } from 'react';
 import EVHubDashboard from './components/dashboard/EVHubDashboard';
-import EVRegistration from './components/login/Registration';
+import EVRegistration from './components/login/login';
+import Navbar from './components/navbar/navbar';
+import TopHome from './components/home/topHome';
+
 
 const App: React.FC = () => {
   const [count, setCount] = useState<number>(0);
 
   return (
-    <>     
-      <EVRegistration />
+    <>           
+    
+    {/* <EVHubDashboard /> */}
+      {/* <EVAuth/> */}
+
+            <Navbar/>
+            <TopHome/>
     </>
   );
 };
