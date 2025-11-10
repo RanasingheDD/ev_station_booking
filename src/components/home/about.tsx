7043d56de75e693b613d68a70270ecd107603f33
Rashen Perera <rashenperera256@gmail.com>
2025-11-10 20:15:49 +0530

    small change in about.tsx

--- src/components/home/about.tsx
+++ src/components/home/about.tsx
@@ -82,7 +82,7 @@ export default function About(): React.ReactElement {
               The passionate individuals driving EV HUB forward
             </p>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
-              {teamMembers.map((member, index) => (
+              {teamMembers.map((member) => (
                 <TeamMember
                   name={member.name}
                   imageUrl={member.imageUrl}
