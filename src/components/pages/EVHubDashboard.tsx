def4b4b8eaa69bd69509683de475ed6d5e5f2c97
roxchain12345 <roxchain12345@gmail.com>
2025-11-20 11:05:48 +0000

    edit

--- src/components/pages/EVHubDashboard.tsx
+++ src/components/pages/EVHubDashboard.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Power, X } from "lucide-react";
+import { X } from "lucide-react";
 
 const EVHubDashboard: React.FC = () => {
   const [showModal, setShowModal] = useState(false);
